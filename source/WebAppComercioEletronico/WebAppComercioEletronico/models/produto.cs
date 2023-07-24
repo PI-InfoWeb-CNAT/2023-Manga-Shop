75b3f6759fe087c94a4aed392a5003d6d856b98b
João Costa <joaovitorcostaoliveira@gmail.com>
2023-07-24 20:11:44 -0300

    Update produto.cs

--- source/WebAppComercioEletronico/WebAppComercioEletronico/models/produto.cs
+++ source/WebAppComercioEletronico/WebAppComercioEletronico/models/produto.cs
@@ -4,7 +4,7 @@
     {
         public string Id { get; set; }
         public string Nome { get; set; }
-        public string preco { get; set; }
+        public string Preco { get; set; }
         public string Descricao { get; set; }
         public string DataCriacao { get; set; }
 
